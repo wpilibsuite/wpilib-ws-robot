cf0c20a9c09cf746e0f61e464c847aadc17be19b
Zhiquan Yeo <zhiquan.yeo@gmail.com>
2020-11-14 09:13:34 -0500

    Update package name and refs + CI

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ import WPILibWSRobotEndpoint from "./wpilib-ws-robot-endpoint";
 export { WPILibWSRobotBase, DigitalChannelMode };
 export { WPILibWSRobotEndpoint };
 
-export { WPILibWSServerConfig, WPILibWSClientConfig } from "node-wpilib-ws";
+export { WPILibWSServerConfig, WPILibWSClientConfig } from "@wpilib/node-wpilib-ws";
