59868963259b0a1080a107f8def0f1b83cb115e9
Zhiquan Yeo <zhiquan.yeo@gmail.com>
2020-12-02 20:22:23 -0500

    Expose FieldDirection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import WPILibWSRobotBase, { DigitalChannelMode } from "./robot-base";
 import WPILibWSRobotEndpoint from "./wpilib-ws-robot-endpoint";
-import SimDevice from "./sim-device";
+import SimDevice, { FieldDirection } from "./sim-device";
 
 export { WPILibWSRobotBase, DigitalChannelMode };
 export { WPILibWSRobotEndpoint };
-export { SimDevice };
+export { SimDevice, FieldDirection };
 
 export { WPILibWSServerConfig, WPILibWSClientConfig } from "@wpilib/node-wpilib-ws";
