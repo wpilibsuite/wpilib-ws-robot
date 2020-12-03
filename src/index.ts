83878ba970e4317af43225622abd1b05706a0966
Zhiquan Yeo <zhiquan.yeo@gmail.com>
2020-12-02 20:09:21 -0500

    Expose SimDevice properly

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import WPILibWSRobotBase, { DigitalChannelMode } from "./robot-base";
 import WPILibWSRobotEndpoint from "./wpilib-ws-robot-endpoint";
+import SimDevice from "./sim-device";
 
 export { WPILibWSRobotBase, DigitalChannelMode };
 export { WPILibWSRobotEndpoint };
+export { SimDevice };
 
 export { WPILibWSServerConfig, WPILibWSClientConfig } from "@wpilib/node-wpilib-ws";
